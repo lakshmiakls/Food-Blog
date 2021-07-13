fad1d9f8ab284a8e7b7779fd44950794d5e5ea98
lakshmiakls <lakshmiakls25@gmail.com>
2021-07-13 13:00:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const sidebar = document.querySelector(".hide");
+
+const closetab = document.querySelector("#closetab");
+
+console.log(sidebar);
+
+function show() {
+    sidebar.style.visibility = "visible";
+}
+
+function hide() {
+    sidebar.style.visibility = "hidden";
+}
